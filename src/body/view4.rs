a7380057cf7638698b5cd3c1e7f92f9e343f08b0
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-06-19 13:00:48 +0530

    committing files

--- src/body/view4.rs
+++ src/body/view4.rs
@@ -11,32 +11,41 @@ use crate::app::MODE;
 pub fn layout() -> impl Widget<AppState> {
     const FONTSIZE: f64 = 10.0;
     use crate::widgets::list_tile;
+    use druid::Env;
+    use druid::LensExt;
+
+    let initial_data = AppState{
+        window_title: String::from("THIS IS"),
+        // vector: vector![1, 2],
+        // vector: Vector::from(vec![1,2,3,4])
+        vector: (0..50).collect(),
+        families: crate::data::family_data::get_family_data_from_db(),
+        checkbox_data: false
+    };
+
     let mut scrollframe = Flex::column()
         .with_child(list_tile::tile_title_generate("SECTIONS"));
-    use crate::data::family_data::FamilyData;
-    use druid::widget::Button;
-    
-    fn fam_item() -> impl Widget<FamilyData> {
-        let item_label = Label::raw()
-            .lens(FamilyData::head_of_family);
-        let row = Flex::row()
-            .with_spacer(5.0)
-            .with_flex_child(Label::new("👪")
-                .with_text_size(FONTSIZE)
-                .with_text_color(MODE.side_bar_item_icon_color),
-                1.0
-            )
-            .with_flex_child(item_label, 1.0);
-        row       
+    use crate::widgets::responsible_tilebutton::TileButton;
+
+    // tiles = 
+    println!("{:?}", &initial_data.families);
+    let mut list = Flex::column();
+    for i in 0..initial_data.families.len() {
+        let button: TileButton<AppState> = TileButton::new(move |data: &AppState, _: &Env| format!("{:?}", 
+        data.families.get(i).unwrap().head_of_family)); 
+        list.add_flex_child(button, 1.0);
     }
+    
+    
+    
         // .with_flex_child(test_label, 1.0);
     scrollframe.add_flex_child(
         Scroll::new(
-            List::new(fam_item) //.lens(Color)/ }
+            list
         )
         .vertical()
-        .background(MODE.tool_icon_bg)
-        .lens(AppState::families),
+        .background(MODE.tool_icon_bg),
+        // .lens(AppState::families),
         1.0,
     );
     scrollframe
