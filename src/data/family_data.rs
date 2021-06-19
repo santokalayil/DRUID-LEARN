f5cbb017b805ce93f0dc6138471db9e0a60fc3b9
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-06-19 13:00:55 +0530

    committing files

--- src/data/family_data.rs
+++ src/data/family_data.rs
@@ -1,66 +1,42 @@
 use sqlite;
-use druid::{im::Vector, Data, Lens};
+use druid::{Data, Lens}; 
 
 #[derive(Clone, Data, Lens)]
-pub struct FamilyData {
+pub struct Family {
+    pub famid: u32,
     pub head_of_family: String,
-    pub members: String,
+
 }
 
+impl Family {
+    pub fn new(famid: u32, head_of_family: impl Into<String>) -> Self {
+        let head_of_family =head_of_family.into();
+        Self {head_of_family, famid }
+    }
+}
 
-
-pub fn get_family_data_from_db() -> Vector<FamilyData> {
+pub fn get_family_data_from_db() -> Vec<Family> {
     let connection = sqlite::open("sqlite.db").unwrap();
     let statement = format!(r#"
-    select hof.hof,  m.members from (select famid, group_concat(member_name, '{delimiter}') as members
+    select hof.famid, hof.hof,  m.members from (select famid, group_concat(member_name, '{delimiter}') as members
     from members group by famid) as m left join
     (select famid, member_name as hof from members where rltshp = "Self") as hof
     on m.famid = hof.famid;
     "#, delimiter=", ");
-    let mut result: Vector<FamilyData> = Vector::default();
+    let mut result: Vec<Family> = Vec::default();
     connection.iterate(format!("{}", statement), |row| {
         let family = row.iter().map(
             |x| match x.1 {
-                None => "NOt FOUND".to_string(),
+                None => "5555".to_string(),
                 Some(v) => v.to_string()
             }
-        ).collect::<Vec<_>>(); // x.0 for selectinn first item in tuple inside list   | ).collect::<Vec<_>>();
-        println!("{:?}", family);
-        result.push_back(FamilyData { 
-            head_of_family : family.get(0).unwrap().to_string(),
-            members: family.get(1).unwrap().to_string(),
+        ).collect::<Vec<_>>(); 
+        result.push(Family { 
+            famid : family.get(0).unwrap().to_string().parse::<u32>().unwrap(), 
+            head_of_family : family.get(1).unwrap().to_string(),
         });
         true
     })
     .unwrap();
     result
 }
-
-// fn get_family_data_from_db() -> Vec<(String, String)>{
-//     let connection = sqlite::open("sqlite.db").unwrap();
-//     let mut result = Vec::new();
-//     let statement = format!(r#"
-//     select hof.hof,  m.members from (select famid, group_concat(member_name, '{delimiter}') as members
-//     from members group by famid) as m left join
-//     (select famid, member_name as hof from members where rltshp = "Self") as hof
-//     on m.famid = hof.famid;
-//     "#, delimiter=", ");
-//     connection.iterate(format!("{}", statement), |row| {
-//         let family = row.iter().map(
-//             |x| match x.1 {
-//                 None => "NOt FOUND".to_string(),
-//                 Some(v) => v.to_string()
-//             }
-//         ).collect::<Vec<_>>(); // x.0 for selectinn first item in tuple inside list   | ).collect::<Vec<_>>();
-//         println!("{:?}", family);
-//         result.push(
-//             (
-//             family.get(0).unwrap().to_string(), // hof
-//             family.get(1).unwrap().to_string(), // members string comma separated
-//             )
-//         );
-//         true
-//     })
-//     .unwrap();
-//     Ok(result)
-// }
