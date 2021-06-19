b9400c0621ad2e07ed4c18f461aaf1edc254674e
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-06-19 13:00:35 +0530

    committing files

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,9 @@
 use druid::{AppLauncher, WindowDesc, Widget, PlatformError,};
-use druid::im::{Vector, vector};
 
 use crate::colors::{Color, DARK, };
 pub use crate::data::appdata::AppState;
 
-
 pub const MODE:Color = DARK;
-
-
 
 pub struct App {}
 
@@ -16,30 +12,34 @@ impl App {
         App{}
     }
     pub fn run (&self) -> Result<(), PlatformError>{
+        // use crate::colors::sidebarbtn::DARK as THEME;
+        // use crate::colors::sidebarbtn::DARK as BTN_THEME;
+        use std::sync::Arc;
+        // use crate::data::family_data::Family;
         let window_title = "Window Title";
         AppLauncher::with_window(
             WindowDesc::new(self.build_ui())
                 .title(window_title)
                 // .menu(crate::widgets::menu::generate_menu)
-                .window_size(druid::Screen::get_display_rect().size() * 0.4 )
+                .window_size(druid::Screen::get_display_rect().size() * 0.9 )
                 // .set_position(
                 //     (druid::Screen::get_display_rect().size() / 4.0).to_vec2().to_point()
                 // )
-                // .show_titlebar(false)
+                .show_titlebar(false)
         ).launch(
             AppState{
                 window_title: String::from(window_title),
-                // vector: vector![1, 2],
-                // vector: Vector::from(vec![1,2,3,4])
                 vector: (0..50).collect(),
-                families: crate::data::family_data::get_family_data_from_db(),
-                checkbox_data: false
+                families: 
+                    Arc::new(crate::data::family_data::get_family_data_from_db()),
+                selected: None,
+                checkbox_data: false,
             }
         )?;
         Ok(())
     }
 
     pub fn build_ui(&self) -> impl Widget<AppState> {
-        crate::body::view4::layout()
+        crate::body::view3::layout()
     }
 }
