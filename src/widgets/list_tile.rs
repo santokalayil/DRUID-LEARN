b2057ab3958d06dc7950a03b21835285a0f76054
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-06-19 13:01:00 +0530

    committing files

--- src/widgets/list_tile.rs
+++ src/widgets/list_tile.rs
@@ -1,17 +1,17 @@
 use druid::widget::{Flex, Label};
 use druid::{Widget, WidgetExt};
 use crate::data::appdata::AppState;
-use crate::data::family_data::FamilyData;
+use crate::data::family_data::Family;
 use crate::app::MODE;
 
 const FONTSIZE: f64 = 10.0;
 
 pub fn family_tile_generate(
-    main_text: druid::widget::LensWrap<FamilyData, String, crate::data::family_data::family_data_derived_lenses::head_of_family, druid::widget::RawLabel<std::string::String>>,
+    main_text: druid::widget::LensWrap<Family, String, crate::data::family_data::family_derived_lenses::head_of_family, druid::widget::RawLabel<std::string::String>>,
     icon:&str,
     left_spacer_size: f64
  )
--> impl Widget<FamilyData> {
+-> impl Widget<Family> {
     Flex::row()
         .with_spacer(left_spacer_size)
         .with_flex_child(Label::new(icon).with_text_size(FONTSIZE), 1.0)
